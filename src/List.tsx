6cc7769325439d524788352c6c6e684448ef70ab
Ahoxa <yujinyuto1223@gmail.com>
2024-12-06 17:31:09 +0900

    feat: make Items draggable

--- src/List.tsx
+++ src/List.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useRef } from "react";
+import invariant from "tiny-invariant";
+import { draggable } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
 import { RiDraggable } from "react-icons/ri";
 
 type ListProps = {
@@ -8,14 +11,32 @@ type ListProps = {
 };
 
 export const List = (props: ListProps) => {
+  const itemRef = useRef<HTMLDivElement | null>(null);
+  const dragHandleRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const el = itemRef.current;
+    const dragHandleEl = dragHandleRef.current;
+    invariant(el);
+    invariant(dragHandleEl);
+
+    draggable({
+      element: el,
+      dragHandle: dragHandleEl,
+    });
+  }, []);
+
   return (
-    <div className="flex items-center p-4 border-2 border-green-200">
-      <div className="cursor-grab p-2">
+    <div
+      ref={itemRef}
+      className="flex items-center p-4 border-2 border-green-200"
+    >
+      <div ref={dragHandleRef} className="cursor-grab p-2">
         <RiDraggable size={"22px"} />
       </div>
       <div className="flex items-center gap-x-4">
         {props.icon}
-        <div className="font-bold ">{props.title}</div>
+        <div className="font-bold">{props.title}</div>
         <div className="">{props.description}</div>
       </div>
     </div>
